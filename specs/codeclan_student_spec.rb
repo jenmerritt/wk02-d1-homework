7cb8b3183c0b56453798655d86abbfe56e51e8a5
Jen Merritt <jenjomez@gmail.com>
2019-11-25 14:27:27 +0000

    upload spec files

---
+++ specs/codeclan_student_spec.rb
@@ -0,0 +1,15 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../bank_account')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestCodeclanStudent < MiniTest::Test
+
+  def test_can_get_student_name
+    student = CodeclanStudent.new("Jen", "E36")
+    assert_equal("Jen", student.student_name())
+  end
+
+
+
+end
